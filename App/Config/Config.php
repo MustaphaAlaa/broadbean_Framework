4f269ac44b23f7a75f4028bac09ce615f0523ead
Mostafa Alaa <anamasry49@gmail.com>
2019-03-23 14:14:47 +0200

    Edit Config

--- App/Config/Config.php
+++ App/Config/Config.php
@@ -2,14 +2,14 @@
 
 // Roots
 define('APPROOT', dirname(__DIR__));
-define('APPNAME', 'Mob</hub>');
+define('APPNAME', 'Your Site Name');
 define('URLROOT', 'URL HERE');
 
 
-//DATABASE
-define('DATABASE', 'mysql');
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '123456');
-define('DBNAME', 'mobile');
-define('CHARSET', 'UTF8');
+//DATABASE 
+define('DATABASE', 'mysql');           	 //   DATABASE
+define('HOST', 'host_name_here');     	//    YOUR HOSTNAME
+define('USER', 'user_name');           //     USERNAME DATABASE
+define('PASSWORD', '###');            //      Your Password
+define('DBNAME', 'dbname');        	 //       Database Name
+define('CHARSET', 'UTF8'); 
