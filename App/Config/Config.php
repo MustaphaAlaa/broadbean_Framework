50d421a11a8f57e68092621f8e41068142658b56
Mostafa Alaa <anamasry49@gmail.com>
2019-03-23 14:24:12 +0200

    Edit Config

--- App/Config/Config.php
+++ App/Config/Config.php
@@ -11,17 +11,17 @@
 
 //        DATABASE SERVER
 	define('DATABASE', 'mysql');
-	   
+
 //       YOUR HOSTNAME        	 
 	define('HOST', 'host_name_here');
-	
+
 //       USERNAME DATABASE     	
 	define('USER', 'user_name'); 
-	 
+
 //       Your Password         
-define('PASSWORD', '###');  
+	define('PASSWORD', '###');  
 //       Database Name          
 	define('DBNAME', 'dbname');
-	
+
 //       Charset
 	define('CHARSET', 'UTF8'); 
