c9031b27bc806e5166d6b4724ed6caaaa8a92eb8
Mostafa Alaa <anamasry49@gmail.com>
2019-03-23 14:20:50 +0200

    Edit Config

--- App/Config/Config.php
+++ App/Config/Config.php
@@ -1,21 +1,22 @@
 <?php
 
-	/*  DIR CONFIG     */
-define('APPROOT', dirname(__DIR__));
-define('APPNAME', 'Your Site Name');
-define('URLROOT', 'URL HERE');
+/*  DIR CONFIG     */
+
+	define('APPROOT', dirname(__DIR__));
+	define('APPNAME', 'Your Site Name');
+	define('URLROOT', 'URL HERE');
 
 
-	/* Database Config */
+/* Database Config */
 
-//   DATABASE SERVER
-define('DATABASE', 'mysql');   
-//    YOUR HOSTNAME        	 
-define('HOST', 'host_name_here');
-//     USERNAME DATABASE     	
-define('USER', 'user_name');  
-//      Your Password         
-define('PASSWORD', '###');  
-//       Database Name          
-define('DBNAME', 'dbname');        
-define('CHARSET', 'UTF8'); 
+	//   DATABASE SERVER
+	define('DATABASE', 'mysql');   
+	//    YOUR HOSTNAME        	 
+	define('HOST', 'host_name_here');
+	//     USERNAME DATABASE     	
+	define('USER', 'user_name');  
+	//      Your Password         
+	define('PASSWORD', '###');  
+	//       Database Name          
+	define('DBNAME', 'dbname');        
+	define('CHARSET', 'UTF8'); 
